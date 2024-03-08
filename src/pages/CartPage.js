cd5f7aa2317d7958a8839f56b4eae11ac4bc440e
thanhledh1 <thanhle.vt2@gmail.com>
2024-03-08 16:25:59 +0700

    update

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -117,34 +117,40 @@ function CartPage(props) {
         </table>
       </div>
       <div className="col-lg-4">
-      
-        <div className="card border-secondary mb-5">
-          <div className="card-header bg-secondary border-0">
-            <h4 className="font-weight-semi-bold m-0">Cart Summary</h4>
-          </div>
-          <div className="card-body">
-            <div className="d-flex justify-content-between mb-3 pt-1">
-              <h6 className="font-weight-medium">Subtotal</h6>
-              <h6 className="font-weight-medium">$150</h6>
+              <div className="card border-secondary mb-5">
+                <div className="card-header bg-secondary border-0">
+                  <h4 className="font-weight-semi-bold m-0">Cart Summary</h4>
+                </div>
+                <div className="card-body">
+                  <div className="d-flex justify-content-between mb-3 pt-1">
+                    <h6 className="font-weight-medium">Subtotal</h6>
+                    <h6 className="font-weight-medium">
+                      ${calculateTotal()}
+                    </h6>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <h6 className="font-weight-medium">Shipping</h6>
+                    <h6 className="font-weight-medium">$10</h6>
+                  </div>
+                </div>
+                <div className="card-footer border-secondary bg-transparent">
+                  <div className="d-flex justify-content-between mt-2">
+                    <h5 className="font-weight-bold">Total</h5>
+                    <h5 className="font-weight-bold">
+                      ${calculateTotal() + 10}
+                    </h5>
+                  </div>
+                  <button
+                    className="btn btn-block btn-primary my-3 py-3"
+                    onClick={handleCheckOut}
+                  >
+                    Proceed To Checkout
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="d-flex justify-content-between">
-              <h6 className="font-weight-medium">Shipping</h6>
-              <h6 className="font-weight-medium">$10</h6>
-            </div>
           </div>
-          <div className="card-footer border-secondary bg-transparent">
-            <div className="d-flex justify-content-between mt-2">
-              <h5 className="font-weight-bold">Total</h5>
-              <h5 className="font-weight-bold">$160</h5>
-            </div>
-            <button className="btn btn-block btn-primary my-3 py-3">
-              Proceed To Checkout
-            </button>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
   {/* Cart End */}
 </>
 
