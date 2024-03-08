e75078fe2a64dd3589c30f31334bb34f61403a9a
thanhledh1 <thanhle.vt2@gmail.com>
2024-03-08 15:23:47 +0700

    a

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -92,7 +92,6 @@ function ProductPage(props) {
                       alt="Image"
                     />
                   </div>
-               
                 </div>
                 <a
                   className="carousel-control-prev"
@@ -121,13 +120,8 @@ function ProductPage(props) {
               {product && product.description}
               </p>
               <div className="d-flex mb-3">
-               
               </div>
               <div className="d-flex mb-4">
-                <p className="text-dark font-weight-medium mb-0 mr-3">
-                  Colors:
-                </p>
-             
               </div>
               <div className="d-flex align-items-center mb-4 pt-2">
                 <div
@@ -178,9 +172,6 @@ function ProductPage(props) {
           <div className="row px-xl-5">
             <div className="col">
               <div className="nav nav-tabs justify-content-center border-secondary mb-4">
-               
-          
-               
               </div>
               <div className="tab-content">
                 <div className="tab-pane fade show active" id="tab-pane-1">
@@ -188,143 +179,12 @@ function ProductPage(props) {
                   <p>
                   {product && product.description_ct}
                   </p>
-                 
                 </div>
                 <div className="tab-pane fade" id="tab-pane-2">
                   <h4 className="mb-3">Additional Information</h4>
                   <p>
                   {product && product.description_ct}
                   </p>
-                  <div className="row">
-                    <div className="col-md-6">
-                      <ul className="list-group list-group-flush">
-                        <li className="list-group-item px-0">
-                          Sit erat duo lorem duo ea consetetur, et eirmod
-                          takimata.
-                        </li>
-                        <li className="list-group-item px-0">
-                          Amet kasd gubergren sit sanctus et lorem eos
-                          sadipscing at.
-                        </li>
-                        <li className="list-group-item px-0">
-                          Duo amet accusam eirmod nonumy stet et et stet eirmod.
-                        </li>
-                        <li className="list-group-item px-0">
-                          Takimata ea clita labore amet ipsum erat justo
-                          voluptua. Nonumy.
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="col-md-6">
-                      <ul className="list-group list-group-flush">
-                        <li className="list-group-item px-0">
-                          Sit erat duo lorem duo ea consetetur, et eirmod
-                          takimata.
-                        </li>
-                        <li className="list-group-item px-0">
-                          Amet kasd gubergren sit sanctus et lorem eos
-                          sadipscing at.
-                        </li>
-                        <li className="list-group-item px-0">
-                          Duo amet accusam eirmod nonumy stet et et stet eirmod.
-                        </li>
-                        <li className="list-group-item px-0">
-                          Takimata ea clita labore amet ipsum erat justo
-                          voluptua. Nonumy.
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div className="tab-pane fade" id="tab-pane-3">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <h4 className="mb-4">
-                        1 review for "Colorful Stylish Shirt"
-                      </h4>
-                      <div className="media mb-4">
-                        <img
-                          src="img/user.jpg"
-                          alt="Image"
-                          className="img-fluid mr-3 mt-1"
-                          style={{ width: 45 }}
-                        />
-                        <div className="media-body">
-                          <h6>
-                            John Doe
-                            <small>
-                              {" "}
-                              - <i>01 Jan 2045</i>
-                            </small>
-                          </h6>
-                          <div className="text-primary mb-2">
-                            <i className="fas fa-star" />
-                            <i className="fas fa-star" />
-                            <i className="fas fa-star" />
-                            <i className="fas fa-star-half-alt" />
-                            <i className="far fa-star" />
-                          </div>
-                          <p>
-                            Diam amet duo labore stet elitr ea clita ipsum,
-                            tempor labore accusam ipsum et no at. Kasd diam
-                            tempor rebum magna dolores sed sed eirmod ipsum.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <h4 className="mb-4">Leave a review</h4>
-                      <small>
-                        Your email address will not be published. Required
-                        fields are marked *
-                      </small>
-                      <div className="d-flex my-3">
-                        <p className="mb-0 mr-2">Your Rating * :</p>
-                        <div className="text-primary">
-                          <i className="far fa-star" />
-                          <i className="far fa-star" />
-                          <i className="far fa-star" />
-                          <i className="far fa-star" />
-                          <i className="far fa-star" />
-                        </div>
-                      </div>
-                      <form>
-                        <div className="form-group">
-                          <label htmlFor="message">Your Review *</label>
-                          <textarea
-                            id="message"
-                            cols={30}
-                            rows={5}
-                            className="form-control"
-                            defaultValue={""}
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label htmlFor="name">Your Name *</label>
-                          <input
-                            type="text"
-                            className="form-control"
-                            id="name"
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label htmlFor="email">Your Email *</label>
-                          <input
-                            type="email"
-                            className="form-control"
-                            id="email"
-                          />
-                        </div>
-                        <div className="form-group mb-0">
-                          <input
-                            type="submit"
-                            defaultValue="Leave Your Review"
-                            className="btn btn-primary px-3"
-                          />
-                        </div>
-                      </form>
-                    </div>
-                  </div>
                 </div>
               </div>
             </div>
@@ -348,126 +208,6 @@ function ProductPage(props) {
                     <img
                       className="img-fluid w-100"
                       src="img/product-1.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                    <h6 className="text-truncate mb-3">
-                      Colorful Stylish Shirt
-                    </h6>
-                    <div className="d-flex justify-content-center">
-                      <h6>$123.00</h6>
-                      <h6 className="text-muted ml-2">
-                        <del>$123.00</del>
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="card-footer d-flex justify-content-between bg-light border">
-                    <a href="" className="btn btn-sm text-dark p-0">
-                      <i className="fas fa-eye text-primary mr-1" />
-                      View Detail
-                    </a>
-                    <a href="" className="btn btn-sm text-dark p-0">
-                      <i className="fas fa-shopping-cart text-primary mr-1" />
-                      Add To Cart
-                    </a>
-                  </div>
-                </div>
-                <div className="card product-item border-0">
-                  <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                    <img
-                      className="img-fluid w-100"
-                      src="img/product-2.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                    <h6 className="text-truncate mb-3">
-                      Colorful Stylish Shirt
-                    </h6>
-                    <div className="d-flex justify-content-center">
-                      <h6>$123.00</h6>
-                      <h6 className="text-muted ml-2">
-                        <del>$123.00</del>
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="card-footer d-flex justify-content-between bg-light border">
-                    <a href="" className="btn btn-sm text-dark p-0">
-                      <i className="fas fa-eye text-primary mr-1" />
-                      View Detail
-                    </a>
-                    <a href="" className="btn btn-sm text-dark p-0">
-                      <i className="fas fa-shopping-cart text-primary mr-1" />
-                      Add To Cart
-                    </a>
-                  </div>
-                </div>
-                <div className="card product-item border-0">
-                  <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                    <img
-                      className="img-fluid w-100"
-                      src="img/product-3.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                    <h6 className="text-truncate mb-3">
-                      Colorful Stylish Shirt
-                    </h6>
-                    <div className="d-flex justify-content-center">
-                      <h6>$123.00</h6>
-                      <h6 className="text-muted ml-2">
-                        <del>$123.00</del>
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="card-footer d-flex justify-content-between bg-light border">
-                    <a href="" className="btn btn-sm text-dark p-0">
-                      <i className="fas fa-eye text-primary mr-1" />
-                      View Detail
-                    </a>
-                    <a href="" className="btn btn-sm text-dark p-0">
-                      <i className="fas fa-shopping-cart text-primary mr-1" />
-                      Add To Cart
-                    </a>
-                  </div>
-                </div>
-                <div className="card product-item border-0">
-                  <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                    <img
-                      className="img-fluid w-100"
-                      src="img/product-4.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                    <h6 className="text-truncate mb-3">
-                      Colorful Stylish Shirt
-                    </h6>
-                    <div className="d-flex justify-content-center">
-                      <h6>$123.00</h6>
-                      <h6 className="text-muted ml-2">
-                        <del>$123.00</del>
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="card-footer d-flex justify-content-between bg-light border">
-                    <a href="" className="btn btn-sm text-dark p-0">
-                      <i className="fas fa-eye text-primary mr-1" />
-                      View Detail
-                    </a>
-                    <a href="" className="btn btn-sm text-dark p-0">
-                      <i className="fas fa-shopping-cart text-primary mr-1" />
-                      Add To Cart
-                    </a>
-                  </div>
-                </div>
-                <div className="card product-item border-0">
-                  <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                    <img
-                      className="img-fluid w-100"
-                      src="img/product-5.jpg"
                       alt=""
                     />
                   </div>
