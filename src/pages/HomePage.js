2efbc17b344b799b54d425b05365b6726533f9c8
thanhledh1 <thanhle.vt2@gmail.com>
2024-03-08 11:27:53 +0700

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,174 +1,190 @@
-// import React, { useEffect, useState } from 'react';
-// import MasterLayout from '../layouts/MasterLayout';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import MasterLayout from '../layouts/MasterLayout';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import Product from '../models/Product';
 
-// function HomePage(props) {
-//     const { id } = useParams()
-//     const [product, setProduct] = useState({})
-//     const cart = useSelector(state => state.cart);
-//     const dispatch = useDispatch();
-//     const navigate = useNavigate();
-//     useEffect(() => {
-//         Product.all()
-//             .then(function (res) {
-//                 // Log data trả về để kiểm tra dữ liệu
-//                 console.log(res);
-//                 setProduct(res.data.data);
-//             })
-//             .catch(function (error) {
-//                 alert("500 error");
-//             });
-//     }, [id]);
+function HomePage(props) {
+    const [products, setProducts] = useState([]);
 
-//     const handleAddtoCart = () => {
-//         let newCart = [...cart];
-//         let isProductExist = false;
+    useEffect(() => {
+        Product.all()
+          .then(function (res) {
+            // Log data trả về để kiểm tra dữ liệu
+            console.log(res.data.data);
+            setProducts(res.data.data);
+          })
+          .catch(function (error) {
+            alert("500 error");
+          });
+      }, []);
 
-//         // Kiểm tra xem sản phẩm đã tồn tại trong giỏ hàng chưa
-//         newCart.forEach(item => {
-//             if (item.product_id === id) {
-//                 // Nếu sản phẩm đã tồn tại, tăng số lượng lên 1
-//                 item.quantity += 1;
-//                 isProductExist = true;
-//             }
-//         });
+  
 
-//         // Nếu sản phẩm chưa tồn tại, thêm sản phẩm mới vào giỏ hàng
-//         if (!isProductExist) {
-//             newCart.push({
-//                 product_id: id,
-//                 quantity: 1,
-//                 product: product
-//             });
-//         }
+    console.log(useParams());
+    return (
+        <MasterLayout>
+            <div className="container-fluid pt-5">
+                <div className="row px-xl-5 pb-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+                        <div
+                            className="d-flex align-items-center border mb-4"
+                            style={{ padding: 30 }}
+                        >
+                            <h1 className="fa fa-check text-primary m-0 mr-3" />
+                            <h5 className="font-weight-semi-bold m-0">Quality Product</h5>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+                        <div
+                            className="d-flex align-items-center border mb-4"
+                            style={{ padding: 30 }}
+                        >
+                            <h1 className="fa fa-shipping-fast text-primary m-0 mr-2" />
+                            <h5 className="font-weight-semi-bold m-0">Free Shipping</h5>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+                        <div
+                            className="d-flex align-items-center border mb-4"
+                            style={{ padding: 30 }}
+                        >
+                            <h1 className="fas fa-exchange-alt text-primary m-0 mr-3" />
+                            <h5 className="font-weight-semi-bold m-0">14-Day Return</h5>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+                        <div
+                            className="d-flex align-items-center border mb-4"
+                            style={{ padding: 30 }}
+                        >
+                            <h1 className="fa fa-phone-volume text-primary m-0 mr-3" />
+                            <h5 className="font-weight-semi-bold m-0">24/7 Support</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-//         dispatch({
-//             type: SET_CART,
-//             payload: newCart
-//         });
+            <div className="container-fluid pt-5">
+                <div className="text-center mb-4">
+                    <h2 className="section-title px-5">
+                        <span className="px-2">Trandy Products</span>
+                    </h2>
+                </div>
+                <div className="row px-xl-5 pb-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+                        <div className="card product-item border-0 mb-4">
+                            <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                <img className="img-fluid w-100" src="img/product-1.jpg" alt="" />
+                            </div>
+                            <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                                <h6 className="text-truncate mb-3">Colorful Stylish Shirt</h6>
+                                <div className="d-flex justify-content-center">
+                                    <h6>$123.00</h6>
+                                    <h6 className="text-muted ml-2">
+                                        <del>$123.00</del>
+                                    </h6>
+                                </div>
+                            </div>
+                            <div className="card-footer d-flex justify-content-between bg-light border">
+                                <a href="" className="btn btn-sm text-dark p-0">
+                                    <i className="fas fa-eye text-primary mr-1" />
+                                    View Detail
+                                </a>
+                                <a href="" className="btn btn-sm text-dark p-0">
+                                    <i className="fas fa-shopping-cart text-primary mr-1" />
+                                    Add To Cart
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-//         // navigate("/cart");
-//     };
+            <div className="container-fluid pt-5">
+<div className="text-center mb-4">
+    <h2 className="section-title px-5">
+        <span className="px-2">Trandy Products</span>
+    </h2>
+</div>
+<div className="row px-xl-5 pb-3">
+    {products.length
+              ? products.map((product, key) => (
+    <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
 
-//     console.log(useParams());
-//     return (
-//         <MasterLayout>
-//             <div className="container-fluid pt-5">
-//                 <div className="row px-xl-5 pb-3">
-//                     <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
-//                         <div
-//                             className="d-flex align-items-center border mb-4"
-//                             style={{ padding: 30 }}
-//                         >
-//                             <h1 className="fa fa-check text-primary m-0 mr-3" />
-//                             <h5 className="font-weight-semi-bold m-0">Quality Product</h5>
-//                         </div>
-//                     </div>
-//                     <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
-//                         <div
-//                             className="d-flex align-items-center border mb-4"
-//                             style={{ padding: 30 }}
-//                         >
-//                             <h1 className="fa fa-shipping-fast text-primary m-0 mr-2" />
-//                             <h5 className="font-weight-semi-bold m-0">Free Shipping</h5>
-//                         </div>
-//                     </div>
-//                     <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
-//                         <div
-//                             className="d-flex align-items-center border mb-4"
-//                             style={{ padding: 30 }}
-//                         >
-//                             <h1 className="fas fa-exchange-alt text-primary m-0 mr-3" />
-//                             <h5 className="font-weight-semi-bold m-0">14-Day Return</h5>
-//                         </div>
-//                     </div>
-//                     <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
-//                         <div
-//                             className="d-flex align-items-center border mb-4"
-//                             style={{ padding: 30 }}
-//                         >
-//                             <h1 className="fa fa-phone-volume text-primary m-0 mr-3" />
-//                             <h5 className="font-weight-semi-bold m-0">24/7 Support</h5>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
 
-//             {/* <div className="container-fluid pt-5">
-//                 <div className="text-center mb-4">
-//                     <h2 className="section-title px-5">
-//                         <span className="px-2">Trandy Products</span>
-//                     </h2>
-//                 </div>
-//                 <div className="row px-xl-5 pb-3">
-//                     <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
-//                         <div className="card product-item border-0 mb-4">
-//                             <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-//                                 <img className="img-fluid w-100" src="img/product-1.jpg" alt="" />
-//                             </div>
-//                             <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-//                                 <h6 className="text-truncate mb-3">Colorful Stylish Shirt</h6>
-//                                 <div className="d-flex justify-content-center">
-//                                     <h6>$123.00</h6>
-//                                     <h6 className="text-muted ml-2">
-//                                         <del>$123.00</del>
-//                                     </h6>
-//                                 </div>
-//                             </div>
-//                             <div className="card-footer d-flex justify-content-between bg-light border">
-//                                 <a href="" className="btn btn-sm text-dark p-0">
-//                                     <i className="fas fa-eye text-primary mr-1" />
-//                                     View Detail
-//                                 </a>
-//                                 <a href="" className="btn btn-sm text-dark p-0">
-//                                     <i className="fas fa-shopping-cart text-primary mr-1" />
-//                                     Add To Cart
-//                                 </a>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div> */}
+        
+        <div className="card product-item border-0 mb-4">
 
-// <div className="container-fluid pt-5">
-//                 <div className="text-center mb-4">
-//                     <h2 className="section-title px-5">
-//                         <span className="px-2">Trandy Products</span>
-//                     </h2>
-//                 </div>
-//                 <div className="row px-xl-5 pb-3">
-//                     <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
-//                         <div className="card product-item border-0 mb-4">
-//                             <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-//                                 <img className="img-fluid w-100" src={product && product.image} alt="" />
-//                             </div>
-//                             <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-//                                 <h6 className="text-truncate mb-3">{product && product.name}</h6>
-//                                 <div className="d-flex justify-content-center">
-//                                     <h6>{product && product.price}</h6>
-//                                     <h6 className="text-muted ml-2">
-//                                         <del>{product && product.price}</del>
-//                                     </h6>
-//                                 </div>
-//                             </div>
-//                             <div className="card-footer d-flex justify-content-between bg-light border">
-//                                 <a href="" className="btn btn-sm text-dark p-0">
-//                                     <i className="fas fa-eye text-primary mr-1" />
-//                                     View Detail
-//                                 </a>
-//                                 <a href="" className="btn btn-sm text-dark p-0" onClick={handleAddtoCart}> 
-//                                     <i className="fas fa-shopping-cart text-primary mr-1" />
-//                                     Add To Cart
-//                                 </a>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
+            
+            <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                <img className="img-fluid w-100"  src={product.image} alt="" />
+            </div>
+            <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                <h6 className="text-truncate mb-3">{product.name}</h6>
+                <div className="d-flex justify-content-center">
+                    <h6>{product.price}</h6>
+                    <h6 className="text-muted ml-2">
+                        <del>$123.00</del>
+                    </h6>
+                </div>
+            </div>
+            <div className="card-footer d-flex justify-content-between bg-light border">
+                <a href="" className="btn btn-sm text-dark p-0">
+                    <i className="fas fa-eye text-primary mr-1" />
+                    View Detail
+                </a>
+                <a href="" className="btn btn-sm text-dark p-0">
+                    <i className="fas fa-shopping-cart text-primary mr-1" />
+                    Add To Cart
+                </a>
+            </div>
+        </div>
+    </div>
+    ))
+    : null}
+</div>
+</div>
 
-//         </MasterLayout>
-//     );
-// }
 
-// export default HomePage;
+{/* <div className="container-fluid pt-5">
+                <div className="text-center mb-4">
+                    <h2 className="section-title px-5">
+                        <span className="px-2">Trandy Products</span>
+                    </h2>
+                </div>
+                <div className="row px-xl-5 pb-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+                        <div className="card product-item border-0 mb-4">
+                            <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                <img className="img-fluid w-100" src={product && product.image} alt="" />
+                            </div>
+                            <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                                <h6 className="text-truncate mb-3">{product && product.name}</h6>
+                                <div className="d-flex justify-content-center">
+                                    <h6>{product && product.price}</h6>
+                                    <h6 className="text-muted ml-2">
+                                        <del>{product && product.price}</del>
+                                    </h6>
+                                </div>
+                            </div>
+                            <div className="card-footer d-flex justify-content-between bg-light border">
+                                <a href="" className="btn btn-sm text-dark p-0">
+                                    <i className="fas fa-eye text-primary mr-1" />
+                                    View Detail
+                                </a>
+                                <a href="" className="btn btn-sm text-dark p-0" onClick={handleAddtoCart}> 
+                                    <i className="fas fa-shopping-cart text-primary mr-1" />
+                                    Add To Cart
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+
+        </MasterLayout>
+    );
+}
+
+export default HomePage;
--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,51 +1,103 @@
-// import React, { useEffect, useState } from "react";
-// import MasterLayout from "../layouts/MasterLayout";
-// import Product from "../models/Product";
-// import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import MasterLayout from "../layouts/MasterLayout";
+import Product from "../models/Product";
+import { Link } from "react-router-dom";
 
-// function ShopPage(props) {
-//   const [products, setProducts] = useState([]);
-//   useEffect(() => {
-//     Product.all()
-//       .then(function (res) {
-//         // Log data trả về để kiểm tra dữ liệu
-//         console.log(res.data.data);
-//         setProducts(res.data.data);
-//       })
-//       .catch(function (error) {
-//         alert("500 error");
-//       });
-//   }, []);
-//   return (
-//     <MasterLayout>
-//       <h1>ShopPage</h1>
-//       <div className="untree_co-section product-section before-footer-section">
-//         <div className="container">
-//           <div className="row">
-//             {/* Start Column 1 */}
+function ShopPage(props) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    Product.all()
+      .then(function (res) {
+        // Log data trả về để kiểm tra dữ liệu
+        console.log(res.data.data);
+        setProducts(res.data.data);
+      })
+      .catch(function (error) {
+        alert("500 error");
+      });
+  }, []);
+  return (
+    <MasterLayout>
+      <h1>ShopPage</h1>
+      <div className="untree_co-section product-section before-footer-section">
+        <div className="container">
+          <div className="row">
+            {/* Start Column 1 */}
 
-//             {products.length
-//               ? products.map((product, key) => (
-//                   <div className="col-12 col-md-4 col-lg-3 mb-5">
-//                     <Link to={`/product/${product.id}`} className="product-item">
-//                       <img
-//                         src={product.image}
-//                         className="img-fluid product-thumbnail"
-//                       />
-//                       <h3 className="product-title">{product.name}</h3>
-//                       <strong className="product-price">{product.price}</strong>
-//                       <span className="icon-cross">
-//                         <img src="images/cross.svg" className="img-fluid" />
-//                       </span>
-//                     </Link>
-//                   </div>
-//                 ))
-//               : null}
-//           </div>
-//         </div>
-//       </div>
-//     </MasterLayout>
-//   );
-// }
+            {products.length
+              ? products.map((product, key) => (
+                  <div className="col-12 col-md-4 col-lg-3 mb-5">
+                    <Link to={`/product/${product.id}`} className="product-item">
+                      <img
+                        src={product.image}
+                        className="img-fluid product-thumbnail"
+                      />
+                      <h3 className="product-title">{product.name}</h3>
+                      <strong className="product-price">{product.price}</strong>
+                      <span className="icon-cross">
+                        <img src="images/cross.svg" className="img-fluid" />
+                      </span>
+                    </Link>
+                  </div>
+                ))
+              : null}
+          </div>
+        </div>
+      </div>
+    </MasterLayout>
+  );
+}
 
-// export default ShopPage;
+
+
+{/* <div className="container-fluid pt-5">
+<div className="text-center mb-4">
+    <h2 className="section-title px-5">
+        <span className="px-2">Trandy Products</span>
+    </h2>
+</div>
+<div className="row px-xl-5 pb-3">
+    <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+
+
+        
+    {products.length
+              ? products.map((product, key) => (
+        <div className="card product-item border-0 mb-4">
+
+            
+            <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                <img className="img-fluid w-100"  src={product.image} alt="" />
+            </div>
+            <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                <h6 className="text-truncate mb-3">{product.name}</h6>
+                <div className="d-flex justify-content-center">
+                    <h6>{product.price}</h6>
+                    <h6 className="text-muted ml-2">
+                        <del>$123.00</del>
+                    </h6>
+                </div>
+            </div>
+            <div className="card-footer d-flex justify-content-between bg-light border">
+                <a href="" className="btn btn-sm text-dark p-0">
+                    <i className="fas fa-eye text-primary mr-1" />
+                    View Detail
+                </a>
+                <a href="" className="btn btn-sm text-dark p-0">
+                    <i className="fas fa-shopping-cart text-primary mr-1" />
+                    Add To Cart
+                </a>
+            </div>
+        </div>
+           ))
+           : null}
+    </div>
+</div>
+</div> */}
+
+
+
+
+
+
+export default ShopPage;
