676e8de6480c2d4e2ef22e49b50ab6d4f941856e
thanhledh1 <thanhle.vt2@gmail.com>
2024-03-12 14:32:42 +0700

    a

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -3,7 +3,6 @@ import MasterLayout from "../layouts/MasterLayout";
 import { useDispatch, useSelector } from "react-redux";
 import OrderModel from "../models/Order";
 import * as Yup from "yup";
-import { Formik, Form, Field, ErrorMessage } from "formik";
 
 function CheckoutPage(props) {
   const cart = useSelector((state) => state.cart);
@@ -16,16 +15,24 @@ function CheckoutPage(props) {
     setUser(login_user);
   }, []);
 
-  const handleSubmit = (values) => {
+  const handleSubmit = () => {
+    const values = {
+      customer_id : user.id,
+      name: user.name,
+      address: user.address,
+      email_address: user.email,
+      phone: user.phone,
+    };
+
     OrderModel.checkout({
       cart: cart,
       userdata: values,
     })
       .then(function (data) {
-        // Xử lý thành công
+        console.log("Order placed successfully:", data);
       })
       .catch(function (error) {
-        // Xử lý lỗi
+        console.error("Error placing order:", error);
       });
   };
 
@@ -33,16 +40,6 @@ function CheckoutPage(props) {
     (accumulator, item) => accumulator + item.product.price * item.quantity,
     0
   );
-
-  const validationSchema = Yup.object().shape({
-    c_fname: Yup.string().required("First Name is required"),
-    c_lname: Yup.string().required("Last Name is required"),
-    c_address: Yup.string().required("Address is required"),
-    c_email_address: Yup.string()
-      .email("Invalid email address")
-      .required("Email Address is required"),
-    c_phone: Yup.string().required("Phone is required"),
-  });
 
   return (
     <MasterLayout>
@@ -68,247 +65,107 @@ function CheckoutPage(props) {
         {/* Page Header End */}
       </>
 
-      <>
-        {/* Checkout Start */}
-        <div className="container-fluid pt-5">
-          <div className="row px-xl-5">
-            <div className="col-lg-8">
-              <div className="mb-4">
-                <h4 className="font-weight-semi-bold mb-4">Billing Address</h4>
-                <div className="row">
-                  <div className="col-md-6 form-group">
-                    <label>First Name</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="John"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Last Name</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="Doe"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>E-mail</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="example@email.com"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Mobile No</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="+123 456 789"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Address Line 1</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="123 Street"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Address Line 2</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="123 Street"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Country</label>
-                    <select className="custom-select">
-                      <option selected="">United States</option>
-                      <option>Afghanistan</option>
-                      <option>Albania</option>
-                      <option>Algeria</option>
-                    </select>
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>City</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="New York"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>State</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="New York"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>ZIP Code</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder={123}
-                    />
-                  </div>
-                  <div className="col-md-12 form-group">
-                    <div className="custom-control custom-checkbox">
-                      <input
-                        type="checkbox"
-                        className="custom-control-input"
-                        id="newaccount"
-                      />
-                      <label
-                        className="custom-control-label"
-                        htmlFor="newaccount"
-                      >
-                        Create an account
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-md-12 form-group">
-                    <div className="custom-control custom-checkbox"></div>
-                  </div>
+      {/* Checkout Start */}
+      <div className="container-fluid pt-5">
+        <div className="row px-xl-5">
+          <div className="col-lg-8">
+            <div className="mb-4">
+              <h4 className="font-weight-semi-bold mb-4">Billing Address</h4>
+              <div className="row">
+                <div className="col-md-6 form-group">
+                  <label className="font-weight-medium">Name customer : </label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    value={user.name}
+                    readOnly
+                  />
                 </div>
-              </div>
-              <div className="collapse mb-4" id="shipping-address">
-                <h4 className="font-weight-semi-bold mb-4">Shipping Address</h4>
-                <div className="row">
-                  <div className="col-md-6 form-group">
-                    <label>First Name</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="John"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Last Name</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="Doe"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>E-mail</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="example@email.com"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Mobile No</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="+123 456 789"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Address Line 1</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="123 Street"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Address Line 2</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="123 Street"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>Country</label>
-                    <select className="custom-select">
-                      <option selected="">United States</option>
-                      <option>Afghanistan</option>
-                      <option>Albania</option>
-                      <option>Algeria</option>
-                    </select>
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>City</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="New York"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>State</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="New York"
-                    />
-                  </div>
-                  <div className="col-md-6 form-group">
-                    <label>ZIP Code</label>
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder={123}
-                    />
-                  </div>
+                <div className="col-md-6 form-group">
+                  <label className="font-weight-medium">Email : </label>
+                  <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    value={user.email}
+                    readOnly
+                  />
                 </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4">
-              <div className="card border-secondary mb-5">
-                <div className="card-header bg-secondary border-0">
-                  <h4 className="font-weight-semi-bold m-0">Order Total</h4>
+                <div className="col-md-6 form-group">
+                  <label className="font-weight-medium">Phone Number : </label>
+                  <input
+                    type="text"
+                    name="phone"
+                    className="form-control"
+                    value={user.phone}
+                    readOnly
+                  />
                 </div>
-                <div className="card-body">
-                  <h5 className="font-weight-medium mb-3">Products</h5>
-                  {cart.map((item, index) => (
-                    <div className="d-flex justify-content-between" key={index}>
-                      <p>{item.product.name}</p>
-                      <strong className="">x</strong> {item.quantity}
-                      <p>${item.product.price}</p>
-                    </div>
-                  ))}
-                  <hr className="mt-0" />
-                  <div className="d-flex justify-content-between mb-3 pt-1">
-                    <h6 className="font-weight-medium">Subtotal</h6>
-                    <h6 className="font-weight-medium">
-                      {" "}
-                      ${cartSubtotal.toFixed()}
-                    </h6>
-                  </div>
-                  <div className="d-flex justify-content-between">
-                    <h6 className="font-weight-medium">Shipping</h6>
-                    <h6 className="font-weight-medium">$10</h6>
-                  </div>
-                </div>
-                <div className="card-footer border-secondary bg-transparent">
-                  <div className="d-flex justify-content-between mt-2">
-                    <h5 className="font-weight-bold">Total</h5>
-                    <h5 className="font-weight-bold">
-                      ${cartSubtotal.toFixed() + 10}
-                    </h5>
-                  </div>
-                  <button className="btn btn-lg btn-block btn-primary font-weight-bold my-3 py-3">
-                    Place Order
-                  </button>
+                <div className="col-md-6 form-group">
+                  <label className="font-weight-medium">Address : </label>
+                  <input
+                    type="text"
+                    name="address"
+                    className="form-control"
+                    value={user.address}
+                    readOnly
+                  />
                 </div>
               </div>
             </div>
           </div>
+
+          <div className="col-lg-4">
+            <div className="card border-secondary mb-5">
+              <div className="card-header bg-secondary border-0">
+                <h4 className="font-weight-semi-bold m-0">Order Total</h4>
+              </div>
+              <div className="card-body">
+                <h5 className="font-weight-medium mb-3">Products</h5>
+                {cart.map((item, index) => (
+                  <div
+                    className="d-flex justify-content-between"
+                    key={index}
+                  >
+                    <p>{item.product.name}</p>
+                    <strong className="">x</strong> {item.quantity}
+                    <p>${item.product.price}</p>
+                  </div>
+                ))}
+                <hr className="mt-0" />
+                <div className="d-flex justify-content-between mb-3 pt-1">
+                  <h6 className="font-weight-medium">Subtotal</h6>
+                  <h6 className="font-weight-medium">
+                    {" "}
+                    ${cartSubtotal.toFixed()}
+                  </h6>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <h6 className="font-weight-medium">Shipping</h6>
+                  <h6 className="font-weight-medium">$10</h6>
+                </div>
+              </div>
+              <div className="card-footer border-secondary bg-transparent">
+                <div className="d-flex justify-content-between mt-2">
+                  <h5 className="font-weight-bold">Total</h5>
+                  <h5 className="font-weight-bold">
+                    ${cartSubtotal.toFixed() + 10}
+                  </h5>
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-lg btn-block btn-primary font-weight-bold my-3 py-3"
+                  onClick={handleSubmit}
+                >
+                  Place Order
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* Checkout End */}
-      </>
+      </div>
+      {/* Checkout End */}
     </MasterLayout>
   );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,10 +3,12 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import MasterLayout from '../layouts/MasterLayout';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage(props) {
   const [message, setMessage] = useState('');
+  const Navigate = useNavigate();
+
 
   const handleSubmit = (values, { setSubmitting, resetForm }) => {
     axios
@@ -18,6 +20,7 @@ function LoginPage(props) {
         localStorage.setItem('token', token);
         setMessage(response.data.message);
         resetForm();
+        Navigate('/checkout');
 
 
         
@@ -42,7 +45,6 @@ function LoginPage(props) {
   return (
     <MasterLayout>
       <div className="container">
-        <h1>Login</h1>
         <Formik
           initialValues={{ email: '', password: '' }}
           validationSchema={validationSchema}
